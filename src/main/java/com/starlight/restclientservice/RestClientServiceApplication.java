2c06dfca15de44956426a94b1850d766491016e8
Andrei Vasileuski <vasileuski.andrei@gmail.com>
2022-06-07 11:40:14 +0300

    add initial files and configurations

---
+++ src/main/java/com/starlight/restclientservice/RestClientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.starlight.restclientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestClientServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starlight/restclientservice/RestClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starlight.restclientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestClientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
